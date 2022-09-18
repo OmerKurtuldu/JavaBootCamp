05bce319086689273abdfabbc50e2eee525998cd
OmerKurtuldu <85666770+OmerKurtuldu@users.noreply.github.com>
2022-09-18 16:32:02 +0300

    Homework

---
+++ Homework1/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Hello World
+		System.out.println("Hello World!");
+		
+		
+		//Variables
+		int numberOfStudents = 9;		
+		String name =  "Number of students = ";
+		System.out.println(name + numberOfStudents);
+		
+		
+		//dataTypes
+		
+		
+		
+
+	}
+
+}
