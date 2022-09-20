da1f5d19f2aebbcb2d9eefa5a6e70f5581722c79
OmerKurtuldu <85666770+OmerKurtuldu@users.noreply.github.com>
2022-09-20 17:30:30 +0300

    odeve devam edildi

--- Homework1/src/Main.java
+++ Homework1/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 
 public class Main {
 
@@ -99,23 +100,99 @@ public class Main {
 			j++;				
 		}while(j<10);
 		
+		System.out.println("DO-While loop finished ");
+		System.out.print("\n");
 		
 		
+		//ARRAYS
+		
+		String[] students = new String[3];
+		students[0] = "Ömer";
+		students[1] = "Ahmet";
+		students[2] = "Mehmet";
+		
+		
+		for (int k = 0; k < students.length; k++) {
+			
+			System.out.println(students[k]);
+		}
+		
+	   System.out.println("------------------------");
+		
+	   for(String student:students) {
+	    	
+	    	System.out.println(student);
+	    	
+	    } 
+	   System.out.println("------------------------");
+	   
+	   
+	   
+	   //ARRAY EXAMPLE
+	   
+	   double[] myList = {1.2,1.6,1.8,5.6};
+	   double sum = 0;
+	   double max = myList[0];
+	   
+	   for(double nmbr:myList) {
+		   if(max<nmbr) {
+			  max = nmbr; 
+		   }
+		   
+		   sum = sum + nmbr;
+		   System.out.println(nmbr);
+	   }
+	   System.out.println("Sum = " + sum);
+	   System.out.println("Max number " + max);
+	   
+		
+	   System.out.println("------------------------");
+	  
+	   //MULTIDIMENSIONAL ARRAY 
+	   
+	   String[][] cities = new String[3][3]; 
+		
+		cities[0][0] = "İstanbul";
+		cities[0][1] = "Sakarya";
+		cities[0][2] = "Kocaeli";
+		cities[1][0] = "Balıkesir";
+		cities[1][1] = "Bursa";
+		cities[1][2] = "Düzce";
+		cities[2][0] = "Tekirdağ";
+		cities[2][1] = "Edirne";
+		cities[2][2] = "Bilecik";
+				
+			
+		for(int s = 0 ;  s<= 2 ; s++){
+			for(int a = 0 ; a <= 2; a++)
+			{
+				System.out.println(cities[s][a]);       
+			}
+		}
+			
+		System.out.println("------------------------");
+		
+		//STRİNG
 			
 		
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+		String message = "The weather is very nice today";
+		System.out.println(message);
 		
+		
+		System.out.println("Number of elements =" + message.length());//Stringin uzunluğunu yazar
+		System.out.println("5th elememt = "+ message.charAt(4));//istediğimiz indexteki elemana ulaşır
+		System.out.println(message.concat(" Wonderful!!"));//ifadenin sonuna istediğimiz metni ekleyebiliriz
+		System.out.println(message.startsWith("T"));//Yazılan harfle başlayıp başlamadığını tru false döndürür
+		System.out.println(message.endsWith("A"));
+		char[] characters = new char [5];
+		message.getChars(0, 5,characters,0);	
+		System.out.println(characters);
+		System.out.println(message.indexOf('a'));
+		
+		String newMessage = message.replace(' ', '_');
+		System.out.println(newMessage);	
 			
+		System.out.println(message.substring(4));	
+		
 	}
-
 }
