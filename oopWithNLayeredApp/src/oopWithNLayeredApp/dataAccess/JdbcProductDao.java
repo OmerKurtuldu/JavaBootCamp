65d88229f9767adf7b6d8e16a26720bf970dc643
OmerKurtuldu <85666770+OmerKurtuldu@users.noreply.github.com>
2022-10-07 22:44:50 +0300

    oopWithNLayeredApp

---
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/JdbcProductDao.java
@@ -0,0 +1,13 @@
+package oopWithNLayeredApp.dataAccess;
+import oopWithNLayeredApp.entities.Product;
+
+public class JdbcProductDao implements ProductDao {
+	
+	public void add(Product product) {
+		
+		//Sadece db erişim kodları buraya yazılır 
+		
+		System.out.println("JDBC ile veritabanına eklendi ");
+		
+	}
+}
